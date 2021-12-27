039b67236d227611681a1f5bb5741e594d34eef1
Hana <hanica11111@gmail.com>
2021-12-27 19:55:50 +0100

    Dodavanje js fileova

---
+++ lupin_pi/src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFec0YGGSXJvmnUxutFYkhmwM5lfCdr5E",
+  authDomain: "lupinproject-da97f.firebaseapp.com",
+  projectId: "lupinproject-da97f",
+  storageBucket: "lupinproject-da97f.appspot.com",
+  messagingSenderId: "1043766608606",
+  appId: "1:1043766608606:web:2677e70e87dfcb2be9f84f",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+let db = firebase.firestore();
+
+export { firebase, db };
