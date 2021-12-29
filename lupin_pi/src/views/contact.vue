33c747e9156e5bd192a05b6eaa447b887a5bbd7c
Hana <hanica11111@gmail.com>
2021-12-29 11:21:25 +0100

    Dodavanje linkova za contact us u footeru

---
+++ lupin_pi/src/views/contact.vue
@@ -0,0 +1,86 @@
+<template>
+  <div style="background-color: #f7f7f7">
+    <navbarbuyer />
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12" style="margin-top: 20px">
+          <h1>Contact Us !</h1>
+        </div>
+        <div class="col-md-12" style="margin-top: 20px">
+          <h2 style="text-align: center">Welcome to Lupin!</h2>
+          <h5 style="text-align: center">Have any questions ???</h5>
+          <img src="@/assets/talking.png" class="center" style="width: 50%" />
+          <p style="font-size: 17px; text-align: center">
+            Don't hesitate to email or call us if you have any queries about the
+            site, advertising, or anything else.
+          </p>
+
+          <img
+            class="center"
+            alt="contact-us"
+            height="87"
+            src="@/assets/email.png"
+            width="320"
+          />
+
+          <i
+            class="fas fa-envelope-open-text center"
+            style="
+              color: #2c3e50;
+              font-size: 20px;
+              text-align: center;
+              margin-top: 10px;
+              margin-bottom: 10px;
+            "
+            ><a
+              href="mailto:lupinauctioning@gmail.com"
+              style="text-decoration: none; color: black"
+              >lupinauctioning@gmail.com</a
+            >
+            <img
+              class="center"
+              alt="contact-us"
+              height="87"
+              src="@/assets/call.png"
+              width="320"
+              style="margin-top: 10px; margin-bottom: 10px"
+            />
+          </i>
+          <i
+            class="fas fa-phone-square-alt fa-lg center"
+            style="text-align: center; margin-top: 5px; margin-bottom: 5px"
+            >0981866607</i
+          >
+          <h3 style="color: #3e005d; text-align: center">
+            We will revert you as soon as possible...!
+          </h3>
+          <p style="text-align: center; color: #3e005d">
+            Thank you for contacting us! <br /><b>Have a great day</b>
+          </p>
+        </div>
+      </div>
+    </div>
+    <footerapp />
+  </div>
+</template>
+
+<script>
+import navbarbuyer from "@/components/navbarbuyer.vue";
+import footerapp from "@/components/footerapp.vue";
+
+export default {
+  name: "Contact",
+  components: {
+    navbarbuyer,
+    footerapp,
+  },
+};
+</script>
+
+<style scoped>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
---
+++ lupin_pi/src/views/sponsors.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <navbarbuyer />
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 style="margin-bottom: 30px; margin-top: 20px">Sponsors</h1>
+        </div>
+        <div class="col-md-12">
+          <h4 style="margin-bottom: 30px">Title sponsor</h4>
+          <img
+            src="@/assets/alliance.png"
+            class="center"
+            style="margin-bottom: 30px"
+          />
+          <h4 style="margin-bottom: 30px">Premium sponsors</h4>
+          <img src="@/assets/premium.png" class="center" />
+          <h4 style="margin-bottom: 30px">Corporate sponsors</h4>
+        </div>
+        <div class="col-md-6" style="text-align: center">
+          <p><i>Choctaw Times</i></p>
+          <p><i>Davis paint and Collision</i></p>
+          <p><i>Dr. Tracy Gasbarra</i></p>
+        </div>
+        <div class="col-md-6" style="text-align: center">
+          <p><i>SWM & Son's</i></p>
+          <p><i>Waste connections</i></p>
+          <p><i>Auction Therapy</i></p>
+        </div>
+      </div>
+    </div>
+    <footerapp />
+  </div>
+</template>
+
+<script>
+import navbarbuyer from "@/components/navbarbuyer.vue";
+import footerapp from "@/components/footerapp.vue";
+export default {
+  name: "Sponsors",
+  components: {
+    navbarbuyer,
+    footerapp,
+  },
+};
+</script>
+
+<style scoped>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
